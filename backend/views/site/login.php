8cb8319ebb2bb7b5d1eab811b794236b10c5608d
Paulo Pereira <ppereiratrabalho@outlook.pt>
2023-12-19 17:08:22 +0000

    basic template

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -4,29 +4,36 @@
 /** @var yii\bootstrap5\ActiveForm $form */
 /** @var \common\models\LoginForm $model */
 
-use yii\bootstrap5\ActiveForm;
+use yii\helpers\Url;
 use yii\bootstrap5\Html;
+use yii\bootstrap5\ActiveForm;
 
 $this->title = 'Login';
 ?>
-<div class="site-login">
-    <div class="mt-5 offset-lg-3 col-lg-6">
-        <h1><?= Html::encode($this->title) ?></h1>
-
-        <p>Please fill out the following fields to login:</p>
-
-        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-            <?= $form->field($model, 'password')->passwordInput() ?>
-
-            <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-            <div class="form-group">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
+<div class="row">
+    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+    <div class="col-lg-6">
+        <div class="p-5">
+            <div class="text-center">
+                <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
             </div>
+            <?php $form = ActiveForm::begin(['id' => 'login-form',
+                'options' => ['class' => 'user']
+            ]); ?>
 
-        <?php ActiveForm::end(); ?>
+            <?= $form->field($model, 'username', ['inputOptions' => ['class' => 'form-control form-control-user', 'placeholder' => 'Enter Your Username']])->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'password', ['inputOptions' => ['class' => 'form-control form-control-user', 'placeholder' => 'Enter Your Password']])->passwordInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'rememberMe')->checkbox() ?>
+            <!-- <a href="index.html" class="btn btn-primary btn-user btn-block">
+                    Login
+                </a> -->
+            <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-user btn-block', 'name' => 'login-button']) ?>
+            <hr>
+            <?php ActiveForm::end() ?>
+            <hr>
+            <div class="text-center">
+                <a class="small" href="<?= Url::to(['/site/forgot-password']) ?>">Forgot Password?</a>
+            </div>
+        </div>
     </div>
 </div>
