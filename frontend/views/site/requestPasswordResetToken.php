b49a8de61b6bd9e3acc2843c5b5a0559bcbd81d4
JorgePereira_95 <jorge.pereira@webcomum.com>
2024-03-18 10:32:35 +0000

    fix: Change Layou

--- frontend/views/site/requestPasswordResetToken.php
+++ frontend/views/site/requestPasswordResetToken.php
@@ -2,30 +2,32 @@
 
 /** @var yii\web\View $this */
 /** @var yii\bootstrap5\ActiveForm $form */
+
 /** @var \frontend\models\PasswordResetRequestForm $model */
 
-use yii\bootstrap5\Html;
 use yii\bootstrap5\ActiveForm;
+use yii\bootstrap5\Html;
 
 $this->title = 'Request password reset';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-request-password-reset">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="row justify-content-center">
+        <div class="col col-lg-5">
+            <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out your email. A link to reset password will be sent there.</p>
+            <p>Please fill out your email. A link to reset password will be sent there.</p>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
 
-                <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+                    <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Send', ['class' => 'btn btn-primary']) ?>
-                </div>
+                    <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
 
-            <?php ActiveForm::end(); ?>
+                    <div class="form-group">
+                        <?= Html::submitButton('Send', ['class' => 'btn btn-primary']) ?>
+                    </div>
+
+                    <?php ActiveForm::end(); ?>
         </div>
     </div>
 </div>
--- frontend/views/site/resendVerificationEmail.php
+++ frontend/views/site/resendVerificationEmail.php
@@ -1,22 +1,22 @@
 <?php
 
-/** @var yii\web\View$this  */
+/** @var yii\web\View $this */
 /** @var yii\bootstrap5\ActiveForm $form */
+
 /** @var \frontend\models\ResetPasswordForm $model */
 
-use yii\bootstrap5\Html;
 use yii\bootstrap5\ActiveForm;
+use yii\bootstrap5\Html;
 
 $this->title = 'Resend verification email';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-resend-verification-email">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out your email. A verification email will be sent there.</p>
-
-    <div class="row">
+    <div class="row justify-content-center">
         <div class="col-lg-5">
+            <h1><?= Html::encode($this->title) ?></h1>
+
+            <p>Please fill out your email. A verification email will be sent there.</p>
             <?php $form = ActiveForm::begin(['id' => 'resend-verification-email-form']); ?>
 
             <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
